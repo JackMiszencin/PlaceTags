b2dd7f27077c06dbd910080680158800c44284ef
Jack <jack.miszencin@gmail.com>
2013-05-02 05:43:33 -0400

    New report and report list on atlas show working

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,10 +3,12 @@ class ReportsController < ApplicationController
 		@reports = Report.all
 	end
 	def new
+		@atlas = Atlas.find(params[:atlas_id])
 		@report = Report.new
+		@report.save
 		respond_to do |format|
-			format.html
-#			format.json { render json: @report }
+			format.html # new.html.erb
+			format.json { render json: @report }
 		end
 	end
 	def create
@@ -14,7 +16,7 @@ class ReportsController < ApplicationController
 		@report.save
 		if request.post?
 			respond_to do |format|
-				format.html { redirect_to @report }
+				format.html { redirect_to atlas_report_path(@report) }
 				format.json {render json: @report, status: :created, location: @report}
 			end
 		else
@@ -27,7 +29,7 @@ class ReportsController < ApplicationController
 
     respond_to do |format|
       if @report.update_attributes(params[:report])
-        format.html { redirect_to @report, notice: 'Atlas was successfully updated.' }
+        format.html { redirect_to atlas_report_path(@report), notice: 'Report was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
@@ -37,7 +39,7 @@ class ReportsController < ApplicationController
   end
   def show
   	@report = Report.find(params[:id])
-
+  	@atlas = @report.atlas
   	respond_to do |format|
   		format.html
   		format.json { render json: @report }
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,5 @@
 class Report < ActiveRecord::Base
+	attr_accessible :location, :event, :atlas_id
 	belongs_to :tag
 	has_and_belongs_to_many :events
 	belongs_to :atlas
