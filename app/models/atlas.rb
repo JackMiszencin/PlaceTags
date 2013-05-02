34bef326f4857d962795cb3e8bf4f0c9330a2c5f
Jack <jack.miszencin@gmail.com>
2013-05-02 01:34:58 -0400

    Database finally working properly

--- app/models/atlas.rb
+++ app/models/atlas.rb
@@ -2,6 +2,7 @@ class Atlas < ActiveRecord::Base
 	attr_accessible :user_id, :name, :sizes, :sizes_attributes
 	belongs_to :user
 	has_many :sizes
+  has_many :reports
 	accepts_nested_attributes_for :sizes, :allow_destroy => true
   
   after_initialize :init
