f2f7d3fdfd1d50a8d5b19765d4cc0df2f08cf06a
Nate Drouin <nate@fundraise.com>
2013-07-01 01:18:25 -0400

    precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ PlaceTags::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  config.assets.precompile += %w( application.css )
 
   # Generate digests for assets URLs
   config.assets.digest = true
